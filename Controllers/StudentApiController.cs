ab7cec24956a0fc52f3dc53d7782f19b8695427e
Keshav Raj Choudhary <Keshavrajchaudhary@t-a-g.net>
2023-09-29 16:07:48 +0530

    Completed the documentation Part Completely

--- Controllers/StudentApiController.cs
+++ Controllers/StudentApiController.cs
@@ -14,11 +14,16 @@ namespace web_api_crud.Controllers
      {
         this.context=context;
      }
-     /// <summary>
-     /// Get All the Student Data
-     /// </summary>
-     /// <returns>GetStudents</returns>
-     [HttpGet]
+      /// <summary>
+      /// Get All Student Data
+      /// </summary>
+      /// <returns>
+      ///   200 OK with a list of student data if students are found.
+      ///   404 Not Found if no students are found.
+      /// </returns>
+      [HttpGet]
+      [ProducesResponseType(StatusCodes.Status200OK)] // Documenting the success response
+      [ProducesResponseType(StatusCodes.Status404NotFound)] // Documenting the error response
      public async Task<ActionResult<List<Student>>> GetStudents()
      {
         var data=await context.Students.ToListAsync();
@@ -27,7 +32,14 @@ namespace web_api_crud.Controllers
       /// <summary>
       /// Get the Particular Data Of Student by their Respective <paramref name="id"/>
       /// </summary>
+      /// <param name="id">The ID of the student to retrieve.</param>
+      /// <returns>
+      ///   200 OK with the student data if the student is found.
+      ///   404 Not Found if the student with the given ID is not found.
+      /// </returns>
       [HttpGet("{id}")]
+      [ProducesResponseType(StatusCodes.Status200OK)] // Documenting the success response
+      [ProducesResponseType(StatusCodes.Status404NotFound)] // Documenting the error response
      public async Task<ActionResult<Student>> GetStudentsById(string id)
      {   
         var student=await context.Students.FindAsync(id);
@@ -38,9 +50,16 @@ namespace web_api_crud.Controllers
         return student;
      }
       /// <summary>
-      /// Create New Student With Data
+      /// Create a New Student with Data
       /// </summary>
+      /// <param name="std">The student data to create.</param>
+      /// <returns>
+      ///   201 Created if the student is successfully created.
+      ///   400 Bad Request if there are validation errors or other issues with the request.
+      /// </returns>
       [HttpPost]
+      [ProducesResponseType(StatusCodes.Status201Created)] // Documenting the success response
+      [ProducesResponseType(StatusCodes.Status400BadRequest)] // Documenting the error response
      public async Task<ActionResult<Student>> CreateStudent(Student std)
      {   
         await context.Students.AddAsync(std);
@@ -48,9 +67,17 @@ namespace web_api_crud.Controllers
         return Ok(std);
      }
       /// <summary>
-      /// Update the Data of Exsiting Student
+      /// Update the Data of Existing Student by <paramref name="id"/>
       /// </summary>
+      /// <param name="id">The ID of the student to update.</param>
+      /// <param name="std">The updated student data.</param>
+      /// <returns>
+      ///   200 OK if the student is successfully updated.
+      ///   400 Bad Request if the provided ID in the URL does not match the student's ID.
+      /// </returns>
       [HttpPut("{id}")]
+      [ProducesResponseType(StatusCodes.Status200OK)] // Documenting the success response
+      [ProducesResponseType(StatusCodes.Status400BadRequest)] // Documenting the error response
      public async Task<ActionResult<Student>> UpdateStudent(string id, Student std)
      {   
         if(id!=std.StudentId)
@@ -61,10 +88,17 @@ namespace web_api_crud.Controllers
         await context.SaveChangesAsync();
         return Ok(std);
      }
-     /// <summary>
-     /// Delete Student Data By <paramref name="id"/>
-     /// </summary>
+      /// <summary>
+      /// Delete Student Data By <paramref name="id"/>
+      /// </summary>
+      /// <param name="id">The ID of the student to delete.</param>
+      /// <returns>
+      ///   200 OK if the student is successfully deleted.
+      ///   404 Not Found if the student with the given ID is not found.
+      /// </returns>
       [HttpDelete("{id}")]
+      [ProducesResponseType(StatusCodes.Status200OK)] // Documenting the success response
+      [ProducesResponseType(StatusCodes.Status404NotFound)] // Documenting the error response
      public async Task<ActionResult<Student>> DeleteStudent(string id)
      {   
         var std=await context.Students.FindAsync(id);
